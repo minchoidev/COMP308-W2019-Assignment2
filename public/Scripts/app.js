a07a2612d34f1a321a1b1a1d81fb1fccc42f3068
Minseok Choi <minchoidev@gmail.com>
2019-02-16 21:04:31 -0500

    Projects page done

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,9 +1,18 @@
+/* Express Portfolio
+   File Name: app.js
+   Author: Minseok Choi
+   StudentID: 300917184
+   Date: 02/16/2019
+   */
+
+
  // IIFE -- Immediately Invoked Function Expression
 (function(){
 
+    // this function is executed when the webpage loaded in first time
     function Start() {
-        console.log(`%c App Started...`, 
-        "font-size: 20px; color: blue; font-weight: bold");
+        console.log(`%c Express Portfolio Started...`, 
+        "color: red; font-weight: bold");
     }
     
     window.addEventListener("load", Start);
