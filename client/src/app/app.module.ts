8d34edb87d2b6ed1f8907e769c63cb27fe80f868
Minseok Choi <minchoidev@gmail.com>
2019-03-29 18:53:14 -0400

    All basic page components Generated

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
+import { HeaderComponent } from './partial/header/header.component';
+import { FooterComponent } from './partial/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    ProductsComponent,
+    ServicesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
